2d3f4bc3a55305fd202ff664b1d8bf2555491b1c
Osama <abdulhaihafeez@gmail.com>
2024-04-12 21:36:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let inputForm = document.querySelector("#tableForm");
+let input = document.querySelector("#inputID");
+let table = document.querySelector(".time-table");
+
+inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let tableOf = input.value;
+
+    table.innerHTML = "";
+
+    for (let i = 1; i <= 12; i++) {
+        //console.log(tableOf + " x " + i + " =", tableOf * i);
+        const message = `${tableOf} X ${i} = ${tableOf * i}`;
+
+        table.innerHTML += `${message} <br>`;
+    }
+});
